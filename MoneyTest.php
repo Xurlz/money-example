a8ecb2fbe3f7c582648b3af6247df2eef63bac60
Charles Sena <charles.sena.sa@gmail.com>
2022-11-14 23:27:05 -0300

    `testFrancMultiplication` fix

--- MoneyTest.php
+++ MoneyTest.php
@@ -6,8 +6,8 @@ class MoneyTest extends TestCase {
   function testFrancMultiplication()
   {
     $five = new Franc(5);
-    $this->assertTrue($five->times(2)->equals(new Dollar(10)));
-    $this->assertTrue($five->times(3)->equals(new Dollar(15)));
+    $this->assertTrue($five->times(2)->equals(new Franc(10)));
+    $this->assertTrue($five->times(3)->equals(new Franc(15)));
   }
   function testDollarMultiplication()
   {
